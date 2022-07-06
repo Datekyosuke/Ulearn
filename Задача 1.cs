dca63c819246d67dc06286fd4b0ece80c45b978d
Datekyosuke <99591187+Datekyosuke@users.noreply.github.com>
2022-07-06 21:42:40 +0300

    Add files via upload

---
+++ Задача 1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ULearn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа а:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите числа b:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            a = -a - b;
+            b = -a - b;
+            a = -a - b;
+            Console.WriteLine($"Число а: {a}");
+            Console.WriteLine($"Число b: {b}");
+            Console.ReadLine();
+        }
+    }
+}
