3ec950ff32112016d2fa5136b2e860e37bbe73cb
Datekyosuke <99591187+Datekyosuke@users.noreply.github.com>
2022-07-08 23:10:19 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
-﻿public static void Main()
+﻿private static string GetMinX(int a, int b, int c)
 {
-    double num1 = +5.5e-2;
-    float num2 = 7.8f;
-    int num3 = 0;
-    long num4 = 2000000000000L;
-    Console.WriteLine(num1);
-    Console.WriteLine(num2);
-    Console.WriteLine(num3);
-    Console.WriteLine(num4);
+	double x;
+	if (a > 0 || (a == 0 && b == 0))
+	{
+		x = (double)(-b) / (2 * a);
+		return x.ToString();
+	}
+	else
+		return "Impossible";
+	// так можно вернуть строковое представление числа
 }
